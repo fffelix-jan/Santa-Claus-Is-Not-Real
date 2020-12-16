d2c8453372079d61448513fd0eca78b7e696c3df
Félix An <cutekids100@gmail.com>
2020-12-15 19:39:52 -0500

    Update SantaNotReal.cpp

--- SantaNotReal.cpp
+++ SantaNotReal.cpp
@@ -1,4 +1,5 @@
 // Santa Is Not Real (C++ because why not?)
+// This is intended to be a placeholder file so GitHub will mark this repo as C++, which might make it easier to find.
 #include <iostream>
 
 int main(void)
