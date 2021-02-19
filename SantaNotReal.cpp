d9506d164351cbcf1a98a0ec9594ffa065e6a575
Félix An <cutekids100@gmail.com>
2021-02-18 21:09:04 -0500

    Delete SantaNotReal.cpp

--- SantaNotReal.cpp
+++
@@ -1,9 +0,0 @@
-// Santa Is Not Real (C++ because why not?)
-// This is intended to be a placeholder file so GitHub will mark this repo as C++, which might make it easier to find.
-#include <iostream>
-
-int main(void)
-{
-    std::cout << "Santa is NOT real!\n";
-    return 0;
-}
